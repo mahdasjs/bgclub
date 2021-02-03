595714acbd4819bb456d22d5e7c44b9dc45b51b2
mohadesejs <mahdasjs@gmail.com>
2021-02-03 18:27:52 +0330

    show location on map

--- frontend/src/eventpage.js
+++ frontend/src/eventpage.js
@@ -28,7 +28,21 @@ import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Favorite from '@material-ui/icons/Favorite';
-import {likeApi,createCommentApi,createLikeApi,delLikeApi} from './api/apis'
+import {likeApi,createCommentApi,createLikeApi,delLikeApi} from './api/apis';
+import Mapir from "mapir-react-component";
+import { LooksOneOutlined } from '@material-ui/icons';
+
+const Map = Mapir.setToken({
+  transformRequest: url => {
+    return {
+      url: url,
+      headers: {
+        "x-api-key": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjVkMjhhOGY5YzRlMzBlZmM3NTFhYjRkYWQ1Y2QyMDczNzllMTViM2ZjOTg3MzljYzIxNTYyYjYwNWRkMzc2YmFlZmIxNWZhY2ZlYjUyNmYwIn0.eyJhdWQiOiI2OTgwIiwianRpIjoiNWQyOGE4ZjljNGUzMGVmYzc1MWFiNGRhZDVjZDIwNzM3OWUxNWIzZmM5ODczOWNjMjE1NjJiNjA1ZGQzNzZiYWVmYjE1ZmFjZmViNTI2ZjAiLCJpYXQiOjE1NzU5NTYyNzUsIm5iZiI6MTU3NTk1NjI3NSwiZXhwIjoxNTc4NDYxODc1LCJzdWIiOiIiLCJzY29wZXMiOlsiYmFzaWMiXX0.Fx_r1Rguxm3Gtp_RDGxSbjhm67w-f_tldO0AHAyr1-L9JkGKgnaVBNWv4_x1qdjk6I6biCXAKpB5jafrUsp8bRS11pz2Tg0G80vaGb891_XF97pT-WGVV3J_H447tiC5JHj7ZSRodOsiVc8EblsX2BmxgewKyHYqs-6YGHYrVro_-xzNRl8EoXzDZtV34HqUWA0IQ5nqhVW39eIWzu6dmySKfSFoLRcOL9-8qC8p2jk9_siki9k3RBt5NVJyl8rOPHASy6yuqABWyeZZV5N8qELqiipP-Ka_zjc0DgrxwSE1AdvxdNDhZO7x7v72X0eM3oWvFMpwGqI5pRzIOpASiw", //Mapir api key
+        "Mapir-SDK": "reactjs"
+      }
+    };
+  }
+});
 class eventpage extends React.Component{
       constructor(){
         super()
@@ -51,7 +65,12 @@ class eventpage extends React.Component{
           likeId:null,
           anchorEl: null,
           openAdd:false,
+          markerArray: [],
+          lat: 35.72,
+          lon: 51.42
         }
+        this.reverseFunction = this.reverseFunction.bind(this);
+
       }
       handlechangeComment = (e) => {
         this.setState({ comment: e.target.value });
@@ -146,6 +165,27 @@ class eventpage extends React.Component{
     handleClosePopUp=()=>{
       this.setState({openMemberPopUp: !this.state.openMemberPopUp})
     }
+    reverseFunction(map, e) {
+
+      var url = `https://map.ir/reverse/no?lat=${this.state.lat}&lon=${this.state.lon}`
+      fetch(url,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjVkMjhhOGY5YzRlMzBlZmM3NTFhYjRkYWQ1Y2QyMDczNzllMTViM2ZjOTg3MzljYzIxNTYyYjYwNWRkMzc2YmFlZmIxNWZhY2ZlYjUyNmYwIn0.eyJhdWQiOiI2OTgwIiwianRpIjoiNWQyOGE4ZjljNGUzMGVmYzc1MWFiNGRhZDVjZDIwNzM3OWUxNWIzZmM5ODczOWNjMjE1NjJiNjA1ZGQzNzZiYWVmYjE1ZmFjZmViNTI2ZjAiLCJpYXQiOjE1NzU5NTYyNzUsIm5iZiI6MTU3NTk1NjI3NSwiZXhwIjoxNTc4NDYxODc1LCJzdWIiOiIiLCJzY29wZXMiOlsiYmFzaWMiXX0.Fx_r1Rguxm3Gtp_RDGxSbjhm67w-f_tldO0AHAyr1-L9JkGKgnaVBNWv4_x1qdjk6I6biCXAKpB5jafrUsp8bRS11pz2Tg0G80vaGb891_XF97pT-WGVV3J_H447tiC5JHj7ZSRodOsiVc8EblsX2BmxgewKyHYqs-6YGHYrVro_-xzNRl8EoXzDZtV34HqUWA0IQ5nqhVW39eIWzu6dmySKfSFoLRcOL9-8qC8p2jk9_siki9k3RBt5NVJyl8rOPHASy6yuqABWyeZZV5N8qELqiipP-Ka_zjc0DgrxwSE1AdvxdNDhZO7x7v72X0eM3oWvFMpwGqI5pRzIOpASiw'
+  
+          }
+        })
+        .then(response => response.json())
+        .then(data => { console.log(data) })
+  
+      const array = [];
+      array.push(<Mapir.Marker
+        coordinates={[51.420470, 35.729054]}
+        anchor="bottom">
+      </Mapir.Marker>);
+      this.setState({ markerArray: array });
+    }
    componentDidMount() {
     this.props.dispatch( selectedEventData(window.location.pathname.split('/')[2]))
     this.props.dispatch( commentData(window.location.pathname.split('/')[2]))
@@ -219,6 +259,15 @@ class eventpage extends React.Component{
                      <Typography className='bgdescription'>
                         {this.props.selectEvent.description}
                       </Typography>
+                      <Mapir
+                center={[this.state.lon, this.state.lat]}
+                Map={Map}
+                onClick={this.reverseFunction}
+                containerStyle={        {  height: '40vh',
+                width: '33vw'}}
+                >
+                {this.state.markerArray}
+              </Mapir>
                   </Grid>
                   <Grid xs={12} sm={12} lg={1}
                   style={{backgroundColor:'#fff' ,marginTop:'30px',marginLeft:30}} >    
